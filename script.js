39c6295846852a1c8acf58c7f70ee32d4731cf12
ahfercen <ahfercen@gmail.com>
2022-10-13 20:06:35 -0400

    Updating display with input

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
+// Display Handling
 let displayValue = '';
- 
 const displayBox = document.querySelector(".display");
 displayBox.textContent = "";
+function updateDisplay(){
+    displayBox.textContent = displayValue;
+}
 
-
-
+// Number Button Handling
 const btns = document.querySelectorAll('.numbers');
 function numberPressed(e){
     displayValue += e.target.dataset.key;
+    updateDisplay();
 }
 btns.forEach(btn => btn.addEventListener('click',numberPressed));
